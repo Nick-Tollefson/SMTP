94223a27c915054ff1dd9f85b36fed9df1d4bffe
Peter <pjw7904@rit.edu>
2015-05-11 13:09:41 -0400

    Newer version that adds inbox support
    This version adds inbox support and other minor tweaks that improve performance for the server.

--- SMTPServer.py
+++ SMTPServer.py
@@ -1,63 +1,111 @@
 import socket, os, threading, time, calendar
 
-print "----SERVER ONLINE AT: " + socket.getfqdn() + "----"
+print "----SMTP SERVER ONLINE | FQDN: " + socket.getfqdn() + "----"
 
 def SMTPServer(connection, address, checkingClient):
 
-    #added for the groups, this is the first message before the server sends its openning
+
+    users = ["Peter", "Nick", "Tyler", "Kyle"]
+    message = ["time","to","from","subject","body"]
 
     if(checkingClient == True):
 
         user = connection.recv(1024)
-        print "logged in as " + user
+        print "C: " + user + " (This is the user trying to connect)"
+
+        if(user in users):
+
+
+            print "\n*NOTICE*: A client [" + user + "] has logged into the server\n"
+
+
+            readingInbox = open(str(user) + "-inbox.txt", "r")
+
+            for eachLine in readingInbox:
+
+                #fucking sleeping fixes everything guys
+                connection.sendall(str(eachLine.strip()))
+                time.sleep(0.5)
+
+            readingInbox.close()
+
+        else:
+
+
+            connection.sendall("535 Authentication Cred. Invalid")
+            print "S: 535 Authentication Cred. Invalid"
+            connection.close()
+            return None
+
+    else:
+
+        print "*\nNOTICE*: a relay server has logged into the server\n"
+
 
     connection.sendall("220 " + str(socket.getfqdn()) + " ESMTP Postfix")
+    print "S: " + "220 " + str(socket.getfqdn()) + " ESMTP Postfix"
 
 
     userInfo = connection.recv(1024)
+    message[0] = str(time.strftime("%a, %d %b %Y %X"))
+    print "C: " + userInfo
     clientHello = userInfo.split(" ")
 
     if(len(clientHello) == 1):
 
-        connection.sendall("250 Hello " + clientHello[0] + ", I am glad to meet you")
+        if(clientHello[0] == "HELO"):
+
+            connection.sendall("250 Hello stranger, I am glad to meet you")
+            print "S: 250 Hello stranger, I am glad to meet you"
+
+        else:
+
+            connection.sendall("500 Command Syntax Error - expecting HELO")
+            print "S: 500 Command Syntax Error - expecting HELO"
+            connection.close()
+            return None
 
     else:
 
         if(clientHello[0] == "HELO"):
 
             connection.sendall("250 Hello " + clientHello[1] + ", I am glad to meet you")
+            print "S: " + "250 Hello " + clientHello[1] + ", I am glad to meet you"
 
         else:
 
-            connection.sendall("500 Command Syntax Error")
+            connection.sendall("500 Command Syntax Error - expecting HELO")
+            print "S: 500 Command Syntax Error - expecting HELO"
+            connection.close()
+            return None
 
 
-    #need this for relay
+
     mailFrom = connection.recv(1024)
+    print "C: " + mailFrom
     gettingFromAddr = mailFrom[mailFrom.find("<") + 1:mailFrom.find(">")]
-    print mailFrom
-    print gettingFromAddr
+    message[2] = str(gettingFromAddr)
     checkMailFrom = gettingFromAddr.split("@")
 
     try:
 
         socket.gethostbyname(checkMailFrom[1])
-        print checkMailFrom[1]
 
     except socket.gaierror:
 
-        connection.sendall("500 Command Syntax Error")
+        connection.sendall('500 Command Syntax Error - Bad "from" server domain')
+        print 'S: 500 Command Syntax Error - Bad "from" server domain'
         connection.close()
         return None
 
     connection.sendall("250 OK")
+    print "S: 250 OK"
 
 
-    #need this for relay
     mailTo = connection.recv(1024)
+    print "C: " + mailTo
     gettingToAddr = mailTo[mailTo.find("<") + 1:mailTo.find(">")]
-    print mailTo
-    print gettingToAddr
+    message[1] = str(gettingToAddr)
     checkMailTo = gettingToAddr.split("@")
 
     try:
@@ -66,58 +114,83 @@ def SMTPServer(connection, address, checkingClient):
 
     except socket.gaierror:
 
-        connection.sendall("500 Command Syntax Error")
+        connection.sendall('500 Command Syntax Error - Bad "To" server domain')
+        print 'S: 500 Command Syntax Error - Bad "To" server domain'
         connection.close()
+        return None
 
 
     connection.sendall("250 OK")
-
+    print "S: 250 OK"
 
 
     confirmMailStart = connection.recv(1024)
+    print "C: " + confirmMailStart
     if(confirmMailStart == "DATA"):
 
         connection.sendall("354 End data with <CR><LF>.<CR><LF>")
+        print "S: 354 End data with <CR><LF>.<CR><LF>"
 
     else:
 
-        connection.sendall("500 Command Syntax Error")
-
+        connection.sendall("500 Command Syntax Error - Expecting DATA")
+        print "S: 500 Command Syntax Error - Expecting DATA"
 
 
     done = False
     contentOfMail = []
 
+    #new
+    placeHolder = 1
+    messageBody = ""
+
     while(not done):
 
         nextLine = connection.recv(1024)
-        print nextLine
+        print "C: " + str(nextLine)
 
         if(nextLine == "."):
 
             connection.sendall("250 Ok: queued as 12345")
+            print "S: 250 Ok: queued as 12345"
 
             ending = connection.recv(1024)
+            print "C: " + ending
             if(ending == "QUIT"):
 
                 connection.sendall("221 Bye")
+                print "S: 221 Bye"
                 connection.close()
                 print "message transaction complete"
+                #new - make sure to strip right newlines as they come in on client!
+                message[4] = messageBody
+
 
             done = True
 
         else:
 
-            contentOfMail.append(nextLine + "\n")
+            contentOfMail.append(nextLine)
+
+            if(nextLine[:9] == "Subject: "):
+
+                subjectTest = nextLine.split("Subject: ")
+                message[3] = subjectTest[1]
+
+            #new stuff
+            if(placeHolder > 4 and nextLine != "."):
+
+                messageBody += nextLine + "\n"
+
+            placeHolder += 1
 
 
-    threading.Thread(target = MailMan, args = (mailFrom, mailTo, checkMailTo, contentOfMail)).start()
+    threading.Thread(target = MailMan, args = (mailFrom, mailTo, checkMailTo, contentOfMail, message)).start()
 
 
 
-def MailMan(mailFrom, mailTo, checkMailTo, contentOfMail):
+def MailMan(mailFrom, mailTo, checkMailTo, contentOfMail, inboxMessage):
 
-    #change this to check To server vs current running server
     if(socket.gethostbyname(checkMailTo[1]) != socket.gethostbyname(socket.gethostname())):
 
         save_path = os.getcwd() + "\\" + "Other"
@@ -131,7 +204,7 @@ def MailMan(mailFrom, mailTo, checkMailTo, contentOfMail):
             writingMessage.write(eachLine)
 
 
-        print "***RELAY CONNECTION BELOW***"
+        print "***Relay Process Initiating***"
 
         HOST = socket.gethostbyname(checkMailTo[1])
         PORT = 44444
@@ -139,8 +212,6 @@ def MailMan(mailFrom, mailTo, checkMailTo, contentOfMail):
         s = s
         s.connect((HOST, PORT))
 
-        #start with a username
-        #s.sendall(socket.gethostname())
 
         serverHello = s.recv(1024)
         checkServerHello = serverHello.split(" ")
@@ -158,6 +229,7 @@ def MailMan(mailFrom, mailTo, checkMailTo, contentOfMail):
 
 
         s.sendall("HELO " + socket.getfqdn())
+        time.sleep(0.5)
         print "C: HELO " + socket.getfqdn()
 
 
@@ -177,6 +249,7 @@ def MailMan(mailFrom, mailTo, checkMailTo, contentOfMail):
 
 
         s.sendall(mailFrom)
+        time.sleep(0.5)
         print "C: " + mailFrom
         mailFromOk = s.recv(1024)
         checkMailFromOk = mailFromOk.split(" ")
@@ -187,14 +260,13 @@ def MailMan(mailFrom, mailTo, checkMailTo, contentOfMail):
 
         else:
 
-
-            print "S: " + mailFromOk + " (it failed at line 184)"
             s.close()
             return None
 
 
 
         s.sendall(mailTo)
+        time.sleep(0.5)
         print "C: " + mailTo
         mailToOk = s.recv(1024)
         checkMailToOk = mailToOk.split(" ")
@@ -212,6 +284,7 @@ def MailMan(mailFrom, mailTo, checkMailTo, contentOfMail):
 
 
         s.sendall("DATA")
+        time.sleep(0.5)
         print "C: DATA"
         readyForData = s.recv(1024)
         checkReadyForData = readyForData.split(" ")
@@ -236,6 +309,7 @@ def MailMan(mailFrom, mailTo, checkMailTo, contentOfMail):
 
 
         s.sendall(".")
+        time.sleep(0.5)
         endOfMessage = s.recv(1024)
         checkEndOfMessage = endOfMessage.split(" ")
 
@@ -252,6 +326,7 @@ def MailMan(mailFrom, mailTo, checkMailTo, contentOfMail):
 
 
         s.sendall("QUIT")
+        time.sleep(0.5)
         print "C: QUIT"
 
 
@@ -281,80 +356,58 @@ def MailMan(mailFrom, mailTo, checkMailTo, contentOfMail):
 
         for eachLine in contentOfMail:
 
-            writingMessage.write(eachLine)
+            #added this for our benifit of reading mail
+            writingMessage.write(eachLine + "\n")
 
 
-    writingMessage.close()
+        writingMessage.close()
+
+        writingToInbox = open(checkMailTo[0] + "-inbox.txt", "a")
+        writingToInbox.write(str(inboxMessage) + "\n")
+        writingToInbox.close()
+
     contentOfMail[:] = []
 
 #--------------------------------------------------------------------------------------------------------------------
 
 def clientConnection():
 
-    print "client socket started"
+    print "Now listening for clients..."
     isClient = True
     HOST = ''
     PORT = 33333
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((HOST,PORT))
     s.listen(1)
-    global users
-    users = ["Peter", "Nick", "Tyler", "Kyle"]
 
     finished = False
 
     while (not finished):
 
         conn, addr = s.accept()
-        print addr
-        print str(addr[0]) + ":" + str(addr[1]) + " has connected (this is a client)"
+        print "\n" + str(addr[0]) + ":" + str(addr[1]) + " has connected (this is a client)"
         threading.Thread(target = SMTPServer, args = (conn, addr, isClient)).start()
 
 
 def serverConnection():
 
-    print "relay socket started"
+    print "Now listening for relay servers..."
     isClient = False
     HOST = ''
     PORT = 44444
     relay = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     relay.bind((HOST,PORT))
     relay.listen(1)
-    global users
-    users = ["Peter", "Nick", "Tyler", "Kyle"]
 
     finished = False
 
     while (not finished):
 
         conn, addr = relay.accept()
-        print str(addr[0]) + ":" + str(addr[1]) + " has connected (this is a server)"
+        print "\n" + str(addr[0]) + ":" + str(addr[1]) + " has connected (this is a server)"
         threading.Thread(target = SMTPServer, args = (conn, addr, isClient)).start()
-
 
 
 
 threading.Thread(target = clientConnection).start()
 threading.Thread(target = serverConnection).start()
-
-
-"""
-HOST = ''
-PORT = 33333
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST,PORT))
-s.listen(1)
-users = ["Peter", "Nick", "Tyler", "Kyle"]
-lock = threading.Lock()
-
-print "----SERVER ONLINE AT: " + socket.getfqdn() + "----"
-
-finished = False
-
-while (not finished):
-
-    conn, addr = s.accept()
-    print addr
-    print str(addr[0]) + ":" + str(addr[1]) + " has connected"
-    threading.Thread(target = SMTPServer, args = (conn, addr)).start()
-"""
